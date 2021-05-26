42836ac6e317778a51df08b9dccb630454b4da53
Vladimir Zabolotnyi <vov4ikf97@gmail.com>
2021-05-26 19:17:44 +0300

    TRNCTP-29 Added WebSecurityConfig Added changes to scripts

--- src/main/resources/db/migration/V1__create_user_schema.sql
+++ src/main/resources/db/migration/V1__create_user_schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE user
     id       INT         NOT NULL AUTO_INCREMENT,
     name     VARCHAR(20) NOT NULL,
     login    VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );

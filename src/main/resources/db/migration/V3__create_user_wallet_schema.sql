a9ef0f16f0f85c4b3424f134e60e9ac4e20db562
shaobig <54940461+shaobig@users.noreply.github.com>
2021-05-27 18:11:35 +0300

    trnctp-31 ver.2: fixed migration scripts and test data

---
+++ src/main/resources/db/migration/V3__create_user_wallet_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user_wallet (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    # DECIMAL(11,2). Syntax analysis:
+    # 11 is the precision - ten billion minus one max; 2 is the accuracy - 99 coins max.
+    money_balance DECIMAL(11,2) NOT NULL DEFAULT 0.0,
+    cryptocurrency_balance DECIMAL(11,2) NOT NULL DEFAULT 0.0,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);

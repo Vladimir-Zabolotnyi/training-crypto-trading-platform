91ff98345c36ae96e0a51833a365f1203e299ad2
Vladimir Zabolotnyi <vov4ikf97@gmail.com>
2021-06-01 18:16:31 +0300

    TRNCTP-47 Added new Test.

--- src/test/java/sigma/training/ctp/service/WalletServiceTest.java
+++ src/test/java/sigma/training/ctp/service/WalletServiceTest.java
@@ -13,6 +13,7 @@ import sigma.training.ctp.persistence.repository.WalletRepository;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,6 +23,7 @@ public class WalletServiceTest {
   private static final BigDecimal MONEY_BALANCE = new BigDecimal("228.13");
   private static final BigDecimal CRYPTOCURRENCY_BALANCE = new BigDecimal("37");
   private static final BigDecimal CRYPTOCURRENCY_AMOUNT = new BigDecimal("20");
+  private static final BigDecimal CRYPTOCURRENCY_AMOUNT_FOR_EXCEPTION = new BigDecimal("40");
   private static final BigDecimal CRYPTOCURRENCY_BALANCE_REDUCED = new BigDecimal("17");
   private static final WalletEntity WALLET = new WalletEntity(new UserEntity(), MONEY_BALANCE, CRYPTOCURRENCY_BALANCE);
   private static final WalletEntity WALLET_AFTER_UPDATE = new WalletEntity(WALLET.getUser(), MONEY_BALANCE, CRYPTOCURRENCY_BALANCE_REDUCED);
@@ -53,5 +55,11 @@ public class WalletServiceTest {
     WalletRestDto actual = service.reduceWalletCryptocurrencyBalanceByUserId(ID, CRYPTOCURRENCY_AMOUNT);
     assertEquals(expected.getCryptocurrencyBalance(), actual.getCryptocurrencyBalance());
 
+  }
+
+  @Test
+  void exceptionReduceWalletCryptocurrencyBalanceByUserId() {
+    when(repository.findWalletEntityByUserId(ID)).thenReturn(WALLET);
+    assertThrows(IllegalArgumentException.class, () -> service.reduceWalletCryptocurrencyBalanceByUserId(ID, CRYPTOCURRENCY_AMOUNT_FOR_EXCEPTION));
   }
 }

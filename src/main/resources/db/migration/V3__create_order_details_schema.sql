1b97c47643eb32b208c53053b0dcb8e173935787
Vladimir Zabolotnyi <vov4ikf97@gmail.com>
2021-06-01 21:42:39 +0300

    TRNCTP-45 Corrected script.

--- src/main/resources/db/migration/V3__create_order_details_schema.sql
+++ src/main/resources/db/migration/V3__create_order_details_schema.sql
@@ -1,9 +1,10 @@
 CREATE TABLE order_details (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     creation_date DATETIME NOT NULL,
-    user_id INT NOT NULL UNIQUE,
+    user_id INT NOT NULL,
     status ENUM('created','canceled','fulfilled') NOT NULL,
     order_type ENUM('buy','sell') NOT NULL,
     cryptocurrency_price DECIMAL(11,2) NOT NULL,
     cryptocurrency_amount DECIMAL(11,2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id));
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);

788c2f8df4a44f70de22a7163491c0f6941c1802
Vladimir Zabolotnyi <vov4ikf97@gmail.com>
2021-06-01 15:59:09 +0300

    TRNCTP-45 Added create_order_details script.

---
+++ src/main/resources/db/migration/V3__create_order_details_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE order_details (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    creation_date DATETIME NOT NULL,
+    user_id INT NOT NULL UNIQUE,
+    status ENUM('created','canceled','fulfilled') NOT NULL,
+    order_type ENUM('buy','sell') NOT NULL,
+    cryptocurrency_price DECIMAL(11,2) NOT NULL,
+    cryptocurrency_amount DECIMAL(11,2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id));

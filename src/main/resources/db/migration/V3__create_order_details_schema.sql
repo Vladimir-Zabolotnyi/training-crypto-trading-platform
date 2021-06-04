09b61cbc013e5823bbd2ef4b6dc0a0fe76e338b0
Vladimir Zabolotnyi <vov4ikf97@gmail.com>
2021-06-04 14:09:52 +0300

    TRNCTP-59 Remove status from request body in the POST sell order

--- src/main/resources/db/migration/V3__create_order_details_schema.sql
+++ src/main/resources/db/migration/V3__create_order_details_schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE order_details (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     user_id INT NOT NULL,
-    order_status ENUM('CREATED','CANCELLED','FULFILLED') NOT NULL,
+    order_status ENUM('CREATED','CANCELLED','FULFILLED') NOT NULL DEFAULT 'CREATED',
     order_type ENUM('BUY','SELL') NOT NULL,
     cryptocurrency_price DECIMAL(11,2) NOT NULL,
     cryptocurrency_amount DECIMAL(11,2) NOT NULL,

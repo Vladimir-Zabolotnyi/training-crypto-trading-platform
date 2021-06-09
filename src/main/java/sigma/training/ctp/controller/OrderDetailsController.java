f21a155637d432fd3542775e20d555ab2aa13e62
Vladimir Zabolotnyi <vov4ikf97@gmail.com>
2021-06-09 14:01:08 +0300

    TRNCTP-88 Add query param instead of path in getOrdersContr

--- src/main/java/sigma/training/ctp/controller/OrderDetailsController.java
+++ src/main/java/sigma/training/ctp/controller/OrderDetailsController.java
@@ -2,6 +2,7 @@ package sigma.training.ctp.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -135,9 +137,9 @@ public class OrderDetailsController {
       content = @Content(mediaType = "text/plain"))
   })
   @ResponseStatus(HttpStatus.OK)
-  @GetMapping(path = "/{orderType}")
+  @GetMapping
   public @ResponseBody
-  List<OrderDetailsRestDto> getAllOrders(@PathVariable("orderType") @Parameter(
+  List<OrderDetailsRestDto> getAllOrders(@RequestParam(name = "orderType") @Parameter(in = ParameterIn.QUERY,
     description = "type of the order",
     schema = @Schema(allowableValues = {"buy", "sell", "SELL", "BUY"})) String orderType) throws NoActiveOrdersFoundException {
 

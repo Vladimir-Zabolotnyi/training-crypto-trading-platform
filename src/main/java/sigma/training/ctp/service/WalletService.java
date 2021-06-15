7f785a02e9a03dcbe05d302004a46ce697a5302e
Vladimir Zabolotnyi <vov4ikf97@gmail.com>
2021-06-15 19:13:06 +0300

    TRNCTP-100

--- src/main/java/sigma/training/ctp/service/WalletService.java
+++ src/main/java/sigma/training/ctp/service/WalletService.java
@@ -39,36 +39,52 @@ public class WalletService {
   }
 
   public WalletEntity subtractWalletCryptocurrencyBalanceByUserId(Long id, BigDecimal cryptocurrencyAmount) throws InsufficientAmountCryptoException {
+    AuditTrail auditTrail = new AuditTrail();
     WalletEntity wallet = repository.findWalletEntityByUserId(id);
     BigDecimal cryptocurrencyBalance = wallet.getCryptocurrencyBalance();
     if (cryptocurrencyBalance.compareTo(cryptocurrencyAmount) < 0) {
       throw new InsufficientAmountCryptoException();
     }
     wallet.setCryptocurrencyBalance(cryptocurrencyBalance.subtract(cryptocurrencyAmount));
+    auditTrail.setUser(wallet.getUser());
+    auditTrail.setDescription("Cryptocurrency balance was subtracted " + cryptocurrencyAmount + " in the wallet with id= " + wallet.getId());
+    auditTrailRepository.save(auditTrail);
     return repository.save(wallet);
   }
 
   public WalletEntity subtractWalletMoneyBalanceByUserId(Long id, BigDecimal cryptocurrencyAmount, BigDecimal cryptocurrencyPrice) throws InsufficientAmountBankCurrencyException {
+    AuditTrail auditTrail = new AuditTrail();
     WalletEntity wallet = repository.findWalletEntityByUserId(id);
     BigDecimal moneyBalance = wallet.getMoneyBalance();
     if (moneyBalance.compareTo(cryptocurrencyAmount.multiply(cryptocurrencyPrice)) < 0) {
       throw new InsufficientAmountBankCurrencyException();
-  }
+    }
     wallet.setMoneyBalance(moneyBalance.subtract(cryptocurrencyAmount.multiply(cryptocurrencyPrice)));
+    auditTrail.setUser(wallet.getUser());
+    auditTrail.setDescription("Money balance was subtracted " + cryptocurrencyAmount.multiply(cryptocurrencyPrice) + " in the wallet with id= " + wallet.getId());
+    auditTrailRepository.save(auditTrail);
     return repository.save(wallet);
   }
 
   public WalletEntity addWalletMoneyBalanceByUserId(Long id, BigDecimal cryptocurrencyAmount, BigDecimal cryptocurrencyPrice) {
+    AuditTrail auditTrail = new AuditTrail();
     WalletEntity wallet = repository.findWalletEntityByUserId(id);
     BigDecimal moneyBalance = wallet.getMoneyBalance();
     wallet.setMoneyBalance(moneyBalance.add(cryptocurrencyAmount.multiply(cryptocurrencyPrice)));
+    auditTrail.setUser(wallet.getUser());
+    auditTrail.setDescription("Money balance was added " + cryptocurrencyAmount.multiply(cryptocurrencyPrice) + " in the wallet with id= " + wallet.getId());
+    auditTrailRepository.save(auditTrail);
     return repository.save(wallet);
   }
 
   public WalletEntity addWalletCryptocurrencyBalanceByUserId(Long id, BigDecimal cryptocurrencyAmount) {
+    AuditTrail auditTrail = new AuditTrail();
     WalletEntity wallet = repository.findWalletEntityByUserId(id);
     BigDecimal cryptocurrencyBalance = wallet.getCryptocurrencyBalance();
     wallet.setCryptocurrencyBalance(cryptocurrencyBalance.add(cryptocurrencyAmount));
+    auditTrail.setUser(wallet.getUser());
+    auditTrail.setDescription("Cryptocurrency balance was added " + cryptocurrencyAmount + " in the wallet with id= " + wallet.getId());
+    auditTrailRepository.save(auditTrail);
     return repository.save(wallet);
   }
 }

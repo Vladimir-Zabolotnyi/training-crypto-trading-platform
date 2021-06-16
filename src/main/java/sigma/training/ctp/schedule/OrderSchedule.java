a8d099e6fc8736051aed3cbeff45cdfe687aeb7b
Vladimir Zabolotnyi <vov4ikf97@gmail.com>
2021-06-16 17:44:20 +0300

    TRNCTP-118

--- src/main/java/sigma/training/ctp/schedule/OrderSchedule.java
+++ src/main/java/sigma/training/ctp/schedule/OrderSchedule.java
@@ -1,6 +1,7 @@
 package sigma.training.ctp.schedule;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import sigma.training.ctp.dto.OrderFilterDto;
@@ -11,7 +12,6 @@ import sigma.training.ctp.exception.OrderNotFoundException;
 import sigma.training.ctp.service.OrderDetailsService;
 
 import java.time.Instant;
-import java.time.temporal.Temporal;
 import java.util.stream.Collectors;
 
 @Component
@@ -19,12 +19,14 @@ public class OrderSchedule {
   @Autowired
   OrderDetailsService orderDetailsService;
 
-  @Scheduled(fixedDelay = 90000L,initialDelay = 60000L)
+  @Value("${schedule.ttl}")
+  private String ttl;
+
+  @Scheduled(fixedDelayString = "${schedule.fixed_delay}",initialDelayString = "${schedule.init_delay}")
   public void scheduleOrderTtl() throws NoActiveOrdersFoundException {
     OrderFilterDto orderFilterDto = new OrderFilterDto();
-//    orderFilterDto.setOrderType("sell");
     orderDetailsService.getAllOrders(orderFilterDto).stream().peek(orderDetailsRestDto -> {
-      if (Instant.now().compareTo(orderDetailsRestDto.getCreationDate().plusMillis(600000000L)) > 0) {
+      if (Instant.now().compareTo(orderDetailsRestDto.getCreationDate().plusMillis(Long.parseLong(ttl))) > 0) {
         try {
           orderDetailsService.cancelOrder(orderDetailsRestDto.getId());
         } catch (OrderNotFoundException | OrderAlreadyCancelledException | OrderAlreadyFulfilledException e) {
